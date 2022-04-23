184a7dd97baab99ddf8136d9d81718ed68d70ab2
amirali26 <amiralishojaee123@gmail.com>
2022-04-23 13:38:47 +0100

    Update User.cs

--- User.cs
+++ User.cs
@@ -22,6 +22,7 @@ namespace Api.Database.Models
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string ImageUrl { get; set; }
+        [ForeignKey("UserApprovalId")]
         public UserApproval UserApproval { get; set; }
         public DateTime CreatedAt { get; set; }
         public ICollection<Account> Accounts { get; set; }
