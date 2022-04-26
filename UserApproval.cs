c9629dbd16b47689673a33e1965be4d462975c99
amirali26 <amiralishojaee123@gmail.com>
2022-04-26 19:18:35 +0100

    Update UserApproval.cs

--- UserApproval.cs
+++ UserApproval.cs
@@ -1,10 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using HotChocolate;
 
 namespace Api.Database.Models
 {
     public class UserApproval
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [GraphQLIgnore]
         public int Id { get; set; }
+        [GraphQLName("id")] public string ExternalId { get; set; }
         public User User { get; set; }
         public int SraNumber { get; set; }
         public DateTime AdmissionDate { get; set; }
